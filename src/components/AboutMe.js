0315b16877cd3863877c86faf90bb2f254ce4beb
Brian Chang <brian.bcyvr@gmail.com>
2021-09-03 11:06:28 -0700

    formatting about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import Self from "../img/personal-pic.jpg";
 
 const AboutMe = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="about-container">
+      <div className="about-format">
+        <span className="main-col">
+          <h2>About Me</h2>
+          <p>
+            Welcome to my personal web page! I am a third year Computer Science
+            student studying at the University of British Columbia (Vancouver).
+            Along with my years of post-secondary education, I have worked as a
+            network support analyst on a previous co-operative experience and
+            have completed various personal projects in languages such as Java,
+            Python, Dart, and more. I am currently seeking opportunities to work
+            in a software developer role.
+          </p>
+          <p>
+            Reach out to <span>Linkedin</span> to contact me!
+          </p>
+        </span>
+        <img src={Self} alt="Self" className="side-photo"/>
+      </div>
+    </div>
+  );
+};
 
-export default AboutMe
+export default AboutMe;
