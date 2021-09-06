9b227278e126be5bb18057e5b888c5fb3ec0ea93
Brian Chang <brian.bcyvr@gmail.com>
2021-09-05 19:48:58 -0700

    format for tech and lang

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,29 +1,24 @@
 import React from "react";
 import Self from "../img/personal-pic.jpg";
-import AboutMeContent from "./AboutMeContent";
 
 const AboutMe = () => {
   return (
-    <div className="about-container">
-      <div className="about-format">
+    <div className="about-format">
       <hr />
-        <h2>About Me</h2>
-        <hr />
-        <img src={Self} alt="Self" className="side-photo" />
-        <div className="main-col">
-          <p>
-            Welcome to my personal web page! I am a third year Computer Science
-            student studying at the University of British Columbia (Vancouver).
-            Along with my years of post-secondary education, I have worked as a
-            network support analyst on a previous co-operative experience and
-            have completed various personal projects in languages such as Java,
-            Python, Dart, and more. I am currently seeking opportunities to work
-            in a software developer role.
-          </p>
-          <p>
-            Reach out to <span>Linkedin</span> to contact me!
-          </p>
-        </div>
+      <h2>About Me</h2>
+      <hr />
+      <img src={Self} alt="Self" className="side-photo" />
+      <div className="main-col">
+        <p>
+          Welcome to my personal web page! I am a third year Computer Science
+          student studying at the University of British Columbia (Vancouver).
+          Along with my years of post-secondary education, I have worked as a
+          network support analyst on a previous co-operative experience and have
+          completed various personal projects in languages such as Java, Python,
+          Dart, and more. I am currently seeking opportunities to work in a
+          software developer role.
+        </p>
+        <p>Reach out to Linkedin to contact me!</p>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
 import AboutMe from "../components/AboutMe";
+import Languages from "../components/Languages";
+import Technologies from "../components/Technologies";
 const Home = () => {
   return (
     <div>
       <AboutMe />
+      <div className="tray-comp">
+          <Languages/>
+          <Technologies/>
+      </div>
     </div>
   );
 };
